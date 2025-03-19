e907bccaa12d7919f44854222b6ee3a5b63220f2
Ardinirmansyah <143554309+Ardinirmansyah@users.noreply.github.com>
2025-03-19 20:27:47 +0700

    Application
    keseluruhan folder aplikasi disini

---
+++ application/views/template/footer.php
@@ -0,0 +1,56 @@
+</div>
+        <!-- /page content -->
+
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="http://localhost/kasir/assets/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+   <script src="http://localhost/kasir/assets/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- FastClick -->
+    <script src="http://localhost/kasir/assets/vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="http://localhost/kasir/assets/vendors/nprogress/nprogress.js"></script>
+    <!-- Chart.js -->
+    <script src="http://localhost/kasir/assets/vendors/Chart.js/dist/Chart.min.js"></script>
+    <!-- jQuery Sparklines -->
+    <script src="http://localhost/kasir/assets/vendors/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+    <!-- morris.js -->
+    <script src="http://localhost/kasir/assets/vendors/raphael/raphael.min.js"></script>
+    <script src="http://localhost/kasir/assets/vendors/morris.js/morris.min.js"></script>
+    <!-- gauge.js -->
+    <script src="http://localhost/kasir/assets/vendors/gauge.js/dist/gauge.min.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="http://localhost/kasir/assets/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+    <!-- Skycons -->
+    <script src="http://localhost/kasir/assets/vendors/skycons/skycons.js"></script>
+    <!-- Flot -->
+    <script src="http://localhost/kasir/assets/vendors/Flot/jquery.flot.js"></script>
+    <script src="http://localhost/kasir/assets/vendors/Flot/jquery.flot.pie.js"></script>
+    <script src="http://localhost/kasir/assets/vendors/Flot/jquery.flot.time.js"></script>
+    <script src="http://localhost/kasir/assets/vendors/Flot/jquery.flot.stack.js"></script>
+    <script src="http://localhost/kasir/assets/vendors/Flot/jquery.flot.resize.js"></script>
+    <!-- Flot plugins -->
+    <script src="http://localhost/kasir/assets/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+    <script src="http://localhost/kasir/assets/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+    <script src="http://localhost/kasir/assets/vendors/flot.curvedlines/curvedLines.js"></script>
+    <!-- DateJS -->
+    <script src="http://localhost/kasir/assets/vendors/DateJS/build/date.js"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="http://localhost/kasir/assets/vendors/moment/min/moment.min.js"></script>
+    <script src="http://localhost/kasir/assets/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="http://localhost/kasir/assets/build/js/custom.min.js"></script>
+
+  </body>
+</html>
